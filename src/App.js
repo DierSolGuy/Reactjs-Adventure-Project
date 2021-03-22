f8024d030023a19ae3457ee89d2a4347c9bd22f1
Sourish Mukherjee <49568460+DierSolGuy@users.noreply.github.com>
2021-03-22 18:38:38 +0530

    Reactjs Adventure Project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+        </Route>
+        
+      </Switch>
+    </Router>
+  
+    
+      
+    </>
+  );
+}
+
+export default App;
