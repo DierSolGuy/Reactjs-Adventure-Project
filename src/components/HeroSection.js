1cc1f04aef3440818aa8398eafd3c115b2305e7c
Sourish Mukherjee <49568460+DierSolGuy@users.noreply.github.com>
2021-03-24 18:33:59 +0530

    Adventure Project Using react js

---
+++ src/components/HeroSection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            <video src="./assets/videos/video-2.mp4" autoPlay loop muted />
+            <h1>ADVENTURE AWAITS</h1>
+            <p> What are you waiting for? </p> 
+            <div className="hero-btns">
+                
+            </div>          
+        </div>
+    )
+}
+
+export default HeroSection
